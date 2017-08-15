7a33dc80c57cb459f5af95f09e7e7b9d346da65b
Frozzzze <ji900@yandex.ru>
2017-08-15 16:56:45 +0300

    Создал интерфейс удалил 2 старых класса

--- src/Factory/CppDeveloper.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class CppDeveloper {
-    void writeCppCode(){
-        System.out.println("C++ Developer write");
-    }
-}
--- src/Factory/JavaDeveloper.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class JavaDeveloper {
-    void writeJavaCode(){
-        System.out.println("Write Java Code");
-    }
-}
