67118bcb6ea5ce2d93fb8cf8a1f89b656327a1f1
Frozzzze <ji900@yandex.ru>
2017-08-16 18:05:37 +0300

    Создал новый класс

---
+++ src/Tutorial/AbstractFactory/Banking/BankingPM.java
@@ -0,0 +1,10 @@
+package Tutorial.AbstractFactory.Banking;
+
+import Tutorial.AbstractFactory.ProjectManager;
+
+public class BankingPM implements ProjectManager {
+    @Override
+    public void manageProject() {
+        System.out.println("Banking PM manages banking project ");
+    }
+}
