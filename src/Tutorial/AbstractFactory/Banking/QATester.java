88172353671ca1c618e10b4cc714edc6d5bb564d
Frozzzze <ji900@yandex.ru>
2017-08-16 18:06:24 +0300

    Создал новый class

---
+++ src/Tutorial/AbstractFactory/Banking/QATester.java
@@ -0,0 +1,10 @@
+package Tutorial.AbstractFactory.Banking;
+
+import Tutorial.AbstractFactory.Tester;
+
+public class QATester implements Tester {
+    @Override
+    public void testCode() {
+        System.out.println("Tester code");
+    }
+}
