fa7e0fac5ef33494af53c8f9b533ebc2c27afb54
Frozzzze <ji900@yandex.ru>
2017-08-24 00:10:46 +0300

    Оба класса наследуються от Program

--- src/Tutorial/Bridge/BankSystem.java
+++ src/Tutorial/Bridge/BankSystem.java
@@ -1,4 +1,12 @@
 package Tutorial.Bridge;
 
-public class BankSystem {
+public class BankSystem extends Program{
+    protected BankSystem(Developer developer) {
+        super(developer);
+    }
+
+    @Override
+    public void developProgram() {
+
+    }
 }
--- src/Tutorial/Bridge/StockExchange.java
+++ src/Tutorial/Bridge/StockExchange.java
@@ -1,4 +1,12 @@
 package Tutorial.Bridge;
 
-public class StockExchange {
+public class StockExchange extends Program{
+    protected StockExchange(Developer developer) {
+        super(developer);
+    }
+
+    @Override
+    public void developProgram() {
+
+    }
 }
