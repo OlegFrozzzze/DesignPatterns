cd4a1d8e2cd0675d8700054e1f24d04fc3c9a15d
Frozzzze <ji900@yandex.ru>
2017-08-24 00:02:49 +0300

    Добавил 2 метода

--- src/Tutorial/Bridge/Program.java
+++ src/Tutorial/Bridge/Program.java
@@ -1,4 +1,12 @@
 package Tutorial.Bridge;
 
+
 public abstract class Program {
+    protected Developer developer;
+
+    protected Program(Developer developer) {
+        this.developer = developer;
+    }
+
+    public abstract void developProgram();
 }
