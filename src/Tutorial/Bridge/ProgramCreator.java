deb34bef98ccb0dfa99ff33669b23d90db2844b4
Frozzzze <ji900@yandex.ru>
2017-08-23 23:52:50 +0300

    Создал класс

---
+++ src/Tutorial/Bridge/ProgramCreator.java
@@ -0,0 +1,10 @@
+package Tutorial.Bridge;
+
+public class ProgramCreator {
+    public static void main(String[] args) {
+        Program[] programs = null;
+        for(Program program: programs){
+            program.developProgram();
+        }
+    }
+}
