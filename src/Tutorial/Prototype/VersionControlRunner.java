3a08091bbdf4bc192a3f0f864f2749ca2f411a72
Frozzzze <ji900@yandex.ru>
2017-08-26 00:03:02 +0300

    Создал новый класс

---
+++ src/Tutorial/Prototype/VersionControlRunner.java
@@ -0,0 +1,9 @@
+package Tutorial.Prototype;
+
+public class VersionControlRunner {
+    public static void main(String[] args) {
+        Project master =  new Project(1,
+                "SuperProject",
+                "SourceCode sourceCode = new SourceCode();\n");
+    }
+}
