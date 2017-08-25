1cbaee88a9b4f0617f3fbf825878ae113e2bfa7c
Frozzzze <ji900@yandex.ru>
2017-08-26 00:04:18 +0300

    Выводим в консоль Создаём копию

--- src/Tutorial/Prototype/VersionControlRunner.java
+++ src/Tutorial/Prototype/VersionControlRunner.java
@@ -5,5 +5,13 @@ public class VersionControlRunner {
         Project master =  new Project(1,
                 "SuperProject",
                 "SourceCode sourceCode = new SourceCode();\n");
+
+        System.out.println(master);
+
+        ProjectFactory factory = new ProjectFactory(master);
+        Project masterClone = factory.coloneProject();
+
+        System.out.println("\n====================\n");
+        System.out.println(masterClone);
     }
 }
