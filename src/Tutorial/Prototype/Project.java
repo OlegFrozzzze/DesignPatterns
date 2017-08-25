d1941b07d7ac7776be051d3562b4e5d617054eb3
Frozzzze <ji900@yandex.ru>
2017-08-25 23:56:24 +0300

    Создал конструктор

--- src/Tutorial/Prototype/Project.java
+++ src/Tutorial/Prototype/Project.java
@@ -4,4 +4,10 @@ public class Project {
     private int id;
     private String projectName;
     private String sourceCode;
+
+    public Project(int id, String projectName, String sourceCode) {
+        this.id = id;
+        this.projectName = projectName;
+        this.sourceCode = sourceCode;
+    }
 }
