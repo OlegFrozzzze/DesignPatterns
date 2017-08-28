97af033bd4d0c8f63ec315dc78e32531ea4044ab
Frozzzze <ji900@yandex.ru>
2017-08-28 19:41:14 +0300

    Исправил ошибку Добавил ArrayList

--- src/Tutorial/Composite/Project.java
+++ src/Tutorial/Composite/Project.java
@@ -4,11 +4,11 @@ public class Project {
     public static void main(String[] args) {
         Team team = new Team();
 
-        Developer firstJavaDEveloper = new JavaDeveloper();
+        Developer firstJavaDeveloper = new JavaDeveloper();
         Developer secondJavaDeveloper = new JavaDeveloper();
         Developer cppDeveloper = new CppDeveloper();
 
-        team.addDeveloper(firstJavaDEveloper);
+        team.addDeveloper(firstJavaDeveloper);
         team.addDeveloper(secondJavaDeveloper);
         team.addDeveloper(cppDeveloper);
 
--- src/Tutorial/Composite/Team.java
+++ src/Tutorial/Composite/Team.java
@@ -1,4 +1,7 @@
 package Tutorial.Composite;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Team {
+    private List<Developer> developers = new ArrayList<Developer>();
 }
