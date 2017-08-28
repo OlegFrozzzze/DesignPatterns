8629ed52330c374d95e38e4d84b0bdd85cf88376
Frozzzze <ji900@yandex.ru>
2017-08-28 19:33:32 +0300

    Поместил всё это в метод main

--- src/Tutorial/Composite/Project.java
+++ src/Tutorial/Composite/Project.java
@@ -1,15 +1,17 @@
 package Tutorial.Composite;
 
 public class Project {
-    Team team = new Team();
+    public static void main(String[] args) {
+        Team team = new Team();
 
-    Developer firstJavaDEveloper = new JavaDeveloper();
-    Developer secondJavaDeveloper = new JavaDeveloper();
-    Developer cppDeveloper = new CppDeveloper();
+        Developer firstJavaDEveloper = new JavaDeveloper();
+        Developer secondJavaDeveloper = new JavaDeveloper();
+        Developer cppDeveloper = new CppDeveloper();
 
-    team.addDeveloper(firstJavaDEveloper);
-    team.addDeveloper(secondJavaDeveloper);
-    team.addDeveloper(cppDeveloper);
+        team.addDeveloper(firstJavaDEveloper);
+        team.addDeveloper(secondJavaDeveloper);
+        team.addDeveloper(cppDeveloper);
 
-    team.createProject();
+        team.createProject();
+    }
 }
