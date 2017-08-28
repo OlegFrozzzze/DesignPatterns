e2fa345afaa4c68246724d159a83a09df754712a
Frozzzze <ji900@yandex.ru>
2017-08-28 19:43:20 +0300

    Добавил новый метод

--- src/Tutorial/Composite/Team.java
+++ src/Tutorial/Composite/Team.java
@@ -1,7 +1,13 @@
 package Tutorial.Composite;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
     private List<Developer> developers = new ArrayList<Developer>();
+
+    public void addDeveloper(Developer developer){
+        developers.add(developer);
+    }
+
 }
