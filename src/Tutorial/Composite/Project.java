f475a620ebc791df97afa1eed9e0fd761ec06c6c
Frozzzze <ji900@yandex.ru>
2017-08-28 19:30:50 +0300

    Добавил объекты

--- src/Tutorial/Composite/Project.java
+++ src/Tutorial/Composite/Project.java
@@ -1,4 +1,9 @@
 package Tutorial.Composite;
 
 public class Project {
+    Team team = new Team();
+
+    Developer firstJavaDEveloper = new JavaDeveloper();
+    Developer secondJavaDeveloper = new JavaDeveloper();
+    Developer cppDeveloper = new CppDeveloper();
 }
