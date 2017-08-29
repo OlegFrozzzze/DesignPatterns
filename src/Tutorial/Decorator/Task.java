54ca27ddbab10cbc18cf53d12af1a1e8d3bb3ba4
Frozzzze <ji900@yandex.ru>
2017-08-29 23:44:41 +0300

    Создаю JavaDeveloper И вывожу в консоль

--- src/Tutorial/Decorator/Task.java
+++ src/Tutorial/Decorator/Task.java
@@ -2,6 +2,8 @@ package Tutorial.Decorator;
 
 public class Task {
     public static void main(String[] args) {
+        Developer developer = new JavaDeveloper();
 
+        System.out.println(developer.makeJob());
     }
 }
