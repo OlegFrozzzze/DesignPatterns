06185f7dc267a575b3ed03aaf5da3bc4e012d0f3
Frozzzze <ji900@yandex.ru>
2017-09-01 14:45:10 +0300

    Создал новый метод

--- src/Tutorial/Decorator/JavaTeamLead.java
+++ src/Tutorial/Decorator/JavaTeamLead.java
@@ -4,4 +4,8 @@ public class JavaTeamLead extends DeveloperDecorator{
     public JavaTeamLead(Developer developer) {
         super(developer);
     }
+
+    public String sendWeekReport(){
+        return " Send week report to customer";
+    }
 }
