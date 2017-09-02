e41aaf37a269b0a4e2f7d17f6451b49d0cd9400d
Frozzzze <ji900@yandex.ru>
2017-09-03 00:00:22 +0300

    Created a new method doJobBeforeDeadLine

--- src/Tutorial/Facade/Developer.java
+++ src/Tutorial/Facade/Developer.java
@@ -1,4 +1,11 @@
 package Tutorial.Facade;
 
 public class Developer {
+    public void doJobBeforeDeadLine(BugTracker bugTracker){
+        if(bugTracker.isActiveSprint()){
+            System.out.println("Developer is solving problems...");
+        }else {
+            System.out.println("Developer is reading Habrahabr...");
+        }
+    }
 }
