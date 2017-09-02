61b5552150852acd848cca3e78d4db6c65c04cd7
Frozzzze <ji900@yandex.ru>
2017-09-03 00:08:32 +0300

    announced new objects Created a new method solveProblems

--- src/Tutorial/Facade/Workflow.java
+++ src/Tutorial/Facade/Workflow.java
@@ -1,4 +1,11 @@
 package Tutorial.Facade;
 
 public class Workflow {
+    Developer developer = new Developer();
+    Job job = new Job();
+    BugTracker bugTracker =  new BugTracker();
+
+    public void solveProblems(){
+
+    }
 }
