56e1e9846df2f41e048e06e2f90e743dcfdc74b1
Frozzzze <ji900@yandex.ru>
2017-09-06 00:01:04 +0300

    Created a new class. implements class Developer. Override method writeCode.

---
+++ src/Tutorial/Flyweight/JavaDeveloper.java
@@ -0,0 +1,9 @@
+package Tutorial.Flyweight;
+
+public class JavaDeveloper implements Developer {
+
+    @Override
+    public void writeCode() {
+        System.out.println("Java Developer write java code");
+    }
+}
