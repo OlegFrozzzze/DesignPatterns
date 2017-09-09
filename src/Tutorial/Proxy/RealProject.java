d4dca3043b44ccf24927351610f9b3d55ca717a8
Frozzzze <ji900@yandex.ru>
2017-09-09 20:20:21 +0300

    Created constructor Created a new method "load"

--- src/Tutorial/Proxy/RealProject.java
+++ src/Tutorial/Proxy/RealProject.java
@@ -3,8 +3,16 @@ package Tutorial.Proxy;
 public class RealProject implements Project{
     private String url;
 
-    @Override
-    public void run() {
+    public RealProject(String url) {
+        this.url = url;
+        load();
+    }
 
+    public void load(){
+        System.out.println("Loading project from" + url+ "...");
+
+    }    @Override
+    public void run() {
+        System.out.println("Running project" + url + "...");
     }
 }
