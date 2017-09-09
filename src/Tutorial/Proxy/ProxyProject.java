899e4a640ff73502810ed96b4de9a02aad0a2c86
Frozzzze <ji900@yandex.ru>
2017-09-09 20:25:54 +0300

    Created field Created constructor

--- src/Tutorial/Proxy/ProxyProject.java
+++ src/Tutorial/Proxy/ProxyProject.java
@@ -1,8 +1,18 @@
 package Tutorial.Proxy;
 
 public class ProxyProject implements Project{
+    private String url;
+    private RealProject realProject;
+
+    public ProxyProject(String url) {
+        this.url = url;
+    }
+
     @Override
     public void run() {
-
+        if (realProject ==  null){
+            realProject = new RealProject(url);
+        }
+        realProject.run();
     }
 }
